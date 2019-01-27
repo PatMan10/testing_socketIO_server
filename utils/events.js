d6f26c8112ee34597d812514fa2727c483807005
PatMan10 <noman@gmail.com>
2019-01-27 17:09:32 +0200

    2nd commit

--- utils/events.js
+++ utils/events.js
@@ -1,16 +1,11 @@
 module.exports = {
   CONNECT: "connect",
-  res_CONNECT: "@connect",
 
   DISCONNECT: "disconnect",
-  res_DISCONNECT: "@disconnect",
 
   com_ECHO: "#echo",
   res_ECHO: "@echo",
 
   com_BELLO: "#bello",
-  res_BELLO: "@bello",
-
-  SUCCESS: "SUCCESS",
-  FAIL: "FAIL"
+  res_BELLO: "@bello"
 };
